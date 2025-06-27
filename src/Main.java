905e41ff74d585d532989b9d7a2cf0ea8226e83f
Tarun Verma <tarunverma.cse18@gmail.com>
2025-06-27 23:52:39 +0530

    dummy commit

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,6 @@ import Service.SingletonService;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.function.*;
 public class Main {
     public static void main(String[] args) {
